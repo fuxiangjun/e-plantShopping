623072cba6e9800031eccaa084d59aeed4a4fc77
Fuxiangjun <fuxiangjun2013@gmail.com>
2025-04-15 16:19:41 -0400

    update cartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,37 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total=0;
+    cart.forEach((item) => {
+    const price = parseFloat(item.cost.substring(1)); // Remove "$" and convert to number
+    total += price * item.quantity;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    return onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity>=1)
+     dispatch(updateQuantity({ name: item.name, quantity: item.quantity -1 })) ;
+
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+     return item.quantity*parseFloat(item.cost.substring(1))
   };
 
   return (
